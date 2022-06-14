bb2e88a103d5fa89180da1b6bd4fece42053f565
joshanaba <anabajoshchi@gmail.com>
2022-06-14 01:11:43 +0100

    added stuff

--- src/components/CountriesContainer.js
+++ src/components/CountriesContainer.js
@@ -1,16 +1,42 @@
 import CountryContainer from './CountryContainer'
-import { AiOutlineSearch } from 'react-icons/ai';
+import { useState, useEffect } from 'react'
+// import { BsSearch } from 'react-icons/bs';
 
 const CountriesContainer = ({countries}) => {
+  const filterItems = [
+    'Europe', 'Asia', 'America', 'Oceania', 'Africa'
+  ]
+  const [region, setRegion] = useState('')
+  const filterBy = (item) => {
+    console.log(item.toLowerCase())
+    item !=='all' ? setRegion(item) : setRegion('')
+  }
   return (
-    <div className='pt-16 pb-16 pr-20 pl-20 flex flex-col gap-20'>
-     <div className='top'>
-      <div className="search-bar flex items-center gap-5">
-          <label for="search">
-            <AiOutlineSearch className="search-icon" />
+    <div className='pt-16 pb-16 pr-20 pl-20 flex flex-col gap-10'>
+     <div className='z-10 h-10 top flex items-center justify-between'>
+      <div className="search-bar flex items-center gap-5 shadow-md p-3.5 rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+          <label htmlFor="search">
+            <span className="material-icons text-2xl">
+              search
+            </span>
           </label>
-          <input type="search" id="search" placeholder='Search country' className='border-0 outline-0' />
+          <input type="search" id="search" placeholder='Search country' className='border-0 outline-0 w-96' />
         </div>
+      <div>
+        <div className="filters relative cursor-pointer items-center w-44">
+          <button className='flex items-center gap-5 min-w-full justify-between transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 shadow-md p-3.5 rounded-md'>
+            <span className="">{region? region : 'Filter by Region'}</span>
+            <span className="material-icons">expand_more</span>
+          </button>
+          <div className='filter-item-ctn flex flex-col absolute top-14 shadow-md rounded-md w-full right-0 left-0 bg-white p-2'>
+          <button className="filter-item text-start p-0.5 pr-3.5 pl-3.5 cursor-pointer" onClick={()=>{filterBy('all')}}>All</button>
+            {filterItems.sort().map((item, index) => {
+              return (<button key={index} className="filter-item text-start p-0.5 pr-3.5 pl-3.5 cursor-pointer" onClick={()=>{filterBy(item)}}>{item}</button>)
+            })}
+          </div>
+        </div>
+      </div>
+        {/* countries.filter(country => country.name.toLowerCase().includes(search.toLowerCase())) */}
      </div>
       <div className="flex flex-wrap gap-10 justify-between">
         { countries.map((country) => 
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         Where in the world?
       </h1>
       </div>
-      <button className="flex items-center gap-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ...">
+      <button className="flex items-center gap-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
         <BsFillMoonFill />
         Dark Mode
       </button>
