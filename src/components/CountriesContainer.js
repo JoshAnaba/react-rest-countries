253b67a7477072a9299d27a8d319941c1d496461
joshanaba <anabajoshchi@gmail.com>
2022-06-23 00:20:24 +0100

    filter by region complete

--- src/components/CountriesContainer.js
+++ src/components/CountriesContainer.js
@@ -5,13 +5,35 @@ import { useState } from 'react';
 
 const CountriesContainer = ({countries}) => {
   const filterItems = [
-    'Europe', 'Asia', 'America', 'Oceania', 'Africa'
+    {
+      name:'Europe',
+      text: 'Europe'
+    },
+    {
+      name: 'Asia',
+      text: 'Asia'
+    },
+    {
+      name: 'Americas',
+      text: 'America'
+    },
+    {
+      name: 'Oceania',
+      text: 'Oceania'
+    },
+    {
+      name: 'Africa',
+      text: 'Africa'
+    }
   ];
   const [region, setRegion] = useState('');
+  const [filteredCountries, setCountryFilter] = useState(countries)
   const [search, setSearch] = useState('');
   const [filterOpen, setFilterOpen] = useState(false);
   const filterBy = (item) => {
     item !=='all' ? setRegion(item) : setRegion('')
+    const regionFilter =item !=='all' ? countries.filter(c => c.region === item) : countries
+    setCountryFilter(regionFilter)
     setFilterOpen(false)
   }
   return (
@@ -30,12 +52,12 @@ const CountriesContainer = ({countries}) => {
           <div className="filters relative cursor-pointer items-center w-44">
             <button className="flex items-center gap-5 min-w-full justify-between transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 shadow-md p-3.5 rounded-md" onClick={()=>{setFilterOpen(!filterOpen)}}>
               <span className="">{region? region : 'Filter by Region'}</span>
-              <span className="material-icons">expand_more</span>
+              <span className="material-icons">{!filterOpen ? 'expand_more': 'expand_less'}</span>
             </button>
-            {filterOpen && <div className="filter-item-ctn flex flex-col absolute top-14 shadow-md rounded-md w-full right-0 left-0 bg-white p-2">
-            <button className="filter-item text-start p-0.5 pr-3.5 pl-3.5 cursor-pointer" onClick={()=>{filterBy('all')}}>All</button>
+            {filterOpen && <div className="filter-item-ctn flex flex-col absolute top-14 shadow-md rounded-md w-full right-0 left-0 bg-white pt-2 pb-2">
+            <button className="filter-item text-start p-0.5 pr-3.5 pl-3.5 cursor-pointer pl-2 pr-2" onClick={()=>{filterBy('all')}}>All</button>
             {filterItems.sort().map((item, index) => {
-              return (<button key={index} className="filter-item text-start p-0.5 pr-3.5 pl-3.5 cursor-pointer" onClick={()=>{filterBy(item)}}>{item}</button>)
+              return (<button key={index} className={`filter-item text-start p-0.5 pr-3.5 pl-3.5 cursor-pointer pl-2 pr-2 ${region === item.name ? 'active-region': ''}`} onClick={()=>{filterBy(item.name)}}>{region === item.name}{item.text}</button>)
             })}
             </div> }
           </div>
@@ -43,7 +65,7 @@ const CountriesContainer = ({countries}) => {
         {/* countries.filter(country => country.name.toLowerCase().includes(search.toLowerCase())) */}
         </div>
         <div className="flex flex-wrap gap-10 justify-between">
-        { countries.map((country) => 
+        { filteredCountries.map((country) => 
           <CountryContainer
             key={country.name.common}
             country={country}
